35c72b336ed9052bbb576c575ca03732d283a004
SergioMartin86 <eienburuu@gmail.com>
2024-04-27 18:45:34 +0200

    Progress with CI

--- waterbox/make-all-cores.sh
+++ waterbox/make-all-cores.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 set -e
 
-cd emulibc && make -f Makefile $1 -j && cd -
-cd libco && make -f Makefile $1 -j && cd -
+cd emulibc && make -f Makefile $1 -j$(nproc) && cd -
+cd libco && make -f Makefile $1 -j$(nproc) && cd -
 cd ares64 && ./make-both.sh $1 && cd -
-cd bsnescore && make -f Makefile $1 -j && cd -
-cd gpgx && make -f Makefile $1 -j && cd -
-cd libsnes && make -f Makefile $1 -j && cd -
-cd melon && make -f Makefile $1 -j && cd -
-cd picodrive && make -f Makefile $1 -j && cd -
-cd snes9x && make -f Makefile $1 -j && cd -
-cd tic80 && make -f Makefile $1 -j && cd -
-cd uzem && make -f Makefile $1 -j && cd -
-cd virtualjaguar && make -f Makefile $1 -j && cd -
+cd bsnescore && make -f Makefile $1 -j$(nproc) && cd -
+cd gpgx && make -f Makefile $1 -j$(nproc) && cd -
+cd libsnes && make -f Makefile $1 -j$(nproc) && cd -
+cd melon && make -f Makefile $1 -j$(nproc) && cd -
+cd picodrive && make -f Makefile $1 -j$(nproc) && cd -
+cd snes9x && make -f Makefile $1 -j$(nproc) && cd -
+cd tic80 && make -f Makefile $1 -j$(nproc) && cd -
+cd uzem && make -f Makefile $1 -j$(nproc) && cd -
+cd virtualjaguar && make -f Makefile $1 -j$(nproc) && cd -
 cd nyma && ./make-all-released-cores.sh $1 && cd -
 
 # this won't include MAME by default, due to the large amount of time it takes to build it
 # to include MAME just do INCLUDE_MAME=1 ./make-all-cores.sh
 if test "$INCLUDE_MAME" ; then
-	cd mame-arcade && make -f Makefile $1 -j && cd -
+	cd mame-arcade && make -f Makefile $1 -j$(nproc) && cd -
 fi
--- waterbox/nyma/make-all-released-cores.sh
+++ waterbox/nyma/make-all-released-cores.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
 
-make -f turbo.mak $1 -j
-make -f hyper.mak $1 -j
-make -f ngp.mak $1 -j
-make -f faust.mak $1 -j
-make -f pcfx.mak $1 -j
-make -f ss.mak $1 -j
-make -f shock.mak $1 -j
+make -f turbo.mak $1 -j$(nproc)
+make -f hyper.mak $1 -j$(nproc)
+make -f ngp.mak $1 -j$(nproc)
+make -f faust.mak $1 -j$(nproc)
+make -f pcfx.mak $1 -j$(nproc)
+make -f ss.mak $1 -j$(nproc)
+make -f shock.mak $1 -j$(nproc)
 # make -f lynx.mak $1 -j
-make -f vb.mak $1 -j
+make -f vb.mak $1 -j$(nproc)
 # make -f wswan.mak $1 -j
